9e98625f28e2b31f2ebdad8a205c3941e1ae97ab
arinjay <arinjaysharma421@gmail.com>
2016-10-30 19:30:44 +0530

    Adding pokemon class

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  Pokedex
+//
+//  Created by Arinjay on 30/10/16.
+//  Copyright © 2016 Arinjay. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name : String!
+    private var _pokedexId : Int!
+    
+    var name: String{
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+
+    
+    init(name: String, pokedexId: Int){
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
